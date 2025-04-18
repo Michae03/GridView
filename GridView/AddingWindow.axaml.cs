62364010753d45b31ac73b874e4ff1077139c6d9
michal <michal.now03@gmail.com>
2025-04-18 15:51:23 +0200

    WYBÓR STANOWISKA NIE DZIAŁA

---
+++ GridView/AddingWindow.axaml.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace GridView;
+
+public partial class AddingWindow : Window
+{
+    public string Name { get; set; } = "";
+    public string Surname { get; set; } = "";
+    public int Age { get; set; } = 0;
+    public string Position { get; set; } = "";
+    public AddingWindow() {
+        InitializeComponent();
+    }
+
+    private void Confirm_OnClick(object? sender, RoutedEventArgs e) { 
+        Name = NameBox.Text;
+        Surname = SurnameBox.Text;
+        if (int.TryParse(AgeBox.Text, out int age)) {
+            Age = age;
+        }else {
+            Age = 0;
+        }
+
+        if (PositionBox.SelectedItem != null)
+        {
+            //DO ZROBIENIA JAK WRÓCE
+        }
+        else {
+            Position = "";
+        }
+       
+        Close();
+        
+    }
+}
--- GridView/MainWindow.axaml.cs
+++ GridView/MainWindow.axaml.cs
@@ -1,31 +1,58 @@
-using System.Collections.Generic;
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+
 namespace GridView;
 
-public class Person
-{
+public class Person {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
+    public int Age { get; set; }
+    public string Position { get; set; }
+    
 
-    public Person(string name, string surname)
-    {
+    public Person(Func<int> getId, string name = "", string surname= "", int age = 0, string position = "") {
+        Id = getId();
         Name = name;
         Surname = surname;
+        Age = age;
+        Position = position;
     }
 }
-public partial class MainWindow : Window
-{
-    public ObservableCollection<Person> Workers { get;}
+
+public partial class MainWindow : Window {
+    public ObservableCollection<Person> Workers { get; }
+
+    public int GetNewId() {
+        int newId = 0;
+        if (Workers.Count != 0) {
+            newId = Workers.Last().Id + 1;
+        }
+        return newId;
+    }
     public MainWindow()
     {
-        var people = new List<Person> 
-        {
-            new Person("Neil", "Armstrong"),
-            new Person("Buzz", "Lightyear"),
-            new Person("James", "Kirk")
-        };
-        Workers = new ObservableCollection<Person>(people);
+        Workers = new ObservableCollection<Person>();
+        DataContext = this;
         InitializeComponent();
+    }
+
+    private async void Add_OnClick(object? sender, RoutedEventArgs e)
+    {
+       AddingWindow addingWindow = new AddingWindow();
+       await addingWindow.ShowDialog(this);
+       Workers.Add(new Person(GetNewId, addingWindow.Name, addingWindow.Surname, addingWindow.Age, addingWindow.Position));
+    }
+
+    private void Delete_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var selectedItem = WorkersDataGrid.SelectedItem as Person;
+        if (selectedItem != null)
+        {
+            Workers.Remove(selectedItem);
+        }
     }
 }
